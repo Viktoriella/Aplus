3b1a658429f5323c2e8697961896d6d2536931ee
viktoria <viktoriella@yandex.ru>
2020-04-27 13:11:26 +0300

    Optim

--- gulpfile.js
+++ gulpfile.js
@@ -3,14 +3,32 @@ var server = require("browser-sync").create();
 var plumber = require("gulp-plumber");
 var postcss = require("gulp-postcss");
 var autoprefixer = require("autoprefixer");
+var minify = require("gulp-csso");
+var rename = require("gulp-rename");
+var imagemin = require("gulp-imagemin");
+var del = require("del");
+var webp = require("gulp-webp");
+var htmlmin = require("gulp-htmlmin");
+var uglify = require("gulp-uglify");
 
-gulp.task("copy", function () {
-  return gulp.src([
-    "source/img/**",
-  ], {
-    base: "source"
-  })
-  .pipe(gulp.dest("build"));
+gulp.task("webp", function() {
+  return gulp.src("source/img/*.{jpg,jpeg,png}")
+  .pipe(webp({quality: 75}))
+  .pipe(gulp.dest("build/img"));
+});
+
+gulp.task("clean", function() {
+  return del("build");
+});
+
+gulp.task("images", function () {
+  return gulp.src("source/img/**")
+  .pipe(imagemin([
+      imagemin.optipng({optimizationLevel: 3}),
+      imagemin.mozjpeg({quality: 75, progressive: true}),
+      imagemin.svgo()
+    ]))
+  .pipe(gulp.dest("build/img"));
 });
 
 gulp.task("style", function() {
@@ -19,12 +37,22 @@ gulp.task("style", function() {
   .pipe(postcss([
         autoprefixer()
     ]))
+  .pipe(minify())
+  .pipe(rename("style.min.css"))
   .pipe(gulp.dest("build/css"))
   .pipe(server.stream());
 });
 
+gulp.task("js", function() {
+  return gulp.src("source/js/**/*.js")
+  .pipe(uglify())
+  .pipe(rename({suffix: ".min"}))
+  .pipe(gulp.dest("build/js"));
+});
+
 gulp.task("html", function() {
   return gulp.src("source/*.html")
+  .pipe(htmlmin({ collapseWhitespace: true }))
   .pipe(gulp.dest("build"));
 });
 
@@ -35,6 +63,8 @@ gulp.task("serve", function() {
   gulp.watch("source/css/**/*.css", gulp.parallel("style"));
   gulp.watch("source/*.html", gulp.parallel("html"))
     .on("change", server.reload);
+  gulp.watch("source/js/**", gulp.parallel("js"))
+    .on("change", server.reload);
 });
 
-gulp.task("build", gulp.series("copy", "serve"));
+gulp.task("build", gulp.series("clean", "webp", "images", "style", "html", "js", "serve"));
